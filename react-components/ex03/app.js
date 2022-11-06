dc4468cc3d5008bafdb9ad631f14ce2ef1fa155a
Serbanescu Alina <stanalinaionela@yahoo.ro>
2022-11-06 15:18:45 +0200

    Tema 24 Oct

--- react-components/ex03/app.js
+++ react-components/ex03/app.js
@@ -22,4 +22,74 @@ const person = {
   },
 };
 
+console.warn(
+  `Folosind Object.values(), afiseaza o lista inversata cu numele complet inversat al prietenilor. `,
+);
 
+const values1 = Object.values(person.friends);
+values1.reverse();
+values1.forEach(({ name, surname }) => {
+  console.log(`${surname} ${name}`);
+});
+
+console.warn(
+  `Afiseaza propozitia: “Prietenii mei sunt Larry, Steven si Carol.” folosind Object.values()`,
+);
+
+console.log(
+  values1.reduce((carry, { name }, index, friends) => {
+    let punctuation = friends.length - 1 === index ? '.' : ', ';
+    punctuation = friends.length - 2 === index ? ' si ' : punctuation;
+    carry = `${carry}${name}${punctuation}`;
+
+    return carry;
+  }, `Prietenii mei sunt `),
+);
+
+console.warn(
+  `Prin aceeasi metoda, afiseaza propozitia: “Diferenta de varsta intre Larry si Dragos este de xxx ani.” etc…`,
+);
+
+const sentences = values1.map(({ name, age }) => {
+  const diff = Math.abs(person.age - age);
+
+  return `Diferenta de varsta intre ${name} si ${person.name} este de ${diff} ani.`;
+});
+
+console.log(sentences.toString());
+
+console.warn(
+  `Prin aceeasi metoda, afiseaza o lista cu numele complet al prietenilor. `,
+);
+
+const listFriends = values1.map(({ name, surname }) => {
+  return ` ${name} ${surname} `;
+});
+
+console.log(listFriends.toString());
+
+console.warn(
+  `In mod similar, afiseaza propozitia  “Larry are xx ani. Steven are …”`,
+);
+
+console.log(
+  values1
+    .map(({ name, age }) => {
+      return `${name} are ${age} ani.`;
+    })
+    .toString(),
+);
+console.warn(
+  `Afiseaza propozitia: “Prietenii mei sunt Larry Larryson, Steven Stevenson si Carol Carolson.” folosind Object.values()`,
+);
+console.log(
+  values1.reduce((carry, { name, surname }, index, friends) => {
+    let punctuation = ', ';
+    punctuation = friends.length - 1 === index ? '.' : punctuation;
+    punctuation = friends.length - 2 === index ? ' si ' : punctuation;
+
+    carry += `${name} ${surname}${punctuation}`;
+
+    return carry;
+  }, 'Prietenii mei sunt '),
+);
--- react-components/ex04/app.js
+++ react-components/ex04/app.js
@@ -27,5 +27,97 @@ const person = {
   },
 };
 
+console.warn(
+  `Folosind Object.entries() pe proprietatea skills, afiseaza abilitatile persoanei daca acestea sunt true. Folosind propozitii de forma: “person.name cunoaste: html.” “person.name cunoaste: javaScript.”`,
+);
 
+Object.entries(person.skills).forEach(([skill, known]) => {
+  if (!known) {
+    return;
+  }
 
+  console.log(`${person.name} cunoaste: ${skill}`);
+});
+
+console.warn(
+  `Prin aceeasi metoda, afiseaza o lista inversata cu numele complet inversat al prietenilor. `,
+);
+
+Object.entries(person.friends)
+  .reverse()
+  .forEach((entry) => {
+    const [_, { name, surname }] = entry;
+
+    console.log(`${surname} ${name}`);
+  });
+
+console.warn(
+  `Afiseaza propozitia: “Prietenii mei sunt Larry, Steven si Carol.” folosind Object.entries()`,
+);
+const sentence = Object.entries(person.friends).reduce(
+  (sentence, [_, { name }], index, friends) => {
+    const punctuation =
+      friends.length - 1 === index
+        ? '.'
+        : friends.length - 2 === index
+        ? ' si '
+        : ', ';
+
+    sentence += name + punctuation;
+
+    return sentence;
+  },
+  'Prietenii mei sunt ',
+);
+
+console.log(sentence);
+
+console.warn(
+  `In mod similar afiseaza mai multe propozitii (cate una per console.log()) care sa afiseze: “Diferenta de varsta intre Larry si Dragos este de xxx ani.” etc…`,
+);
+Object.entries(person.friends).forEach(([_, { name, age }]) => {
+  const diff = Math.abs(age - person.age);
+
+  console.log(
+    `Diferenta de varsta dintre ${name} si ${person.name} este de ${diff} ani.`,
+  );
+});
+
+console.warn(
+  `Folosind Object.entries() pe proprietatea skills, afiseaza toate abilitatile din obiectul skills. `,
+);
+Object.entries(person.skills).forEach(([skill]) => {
+  console.log(skill);
+});
+
+console.warn(
+  `Prin aceeasi metoda, afiseaza o lista cu numele complet al prietenilor. `,
+);
+Object.entries(person.friends).forEach(([, friend]) => {
+  const { name, surname } = friend;
+  console.log(`${surname} ${name}`);
+});
+
+console.warn(
+  `Afiseaza propozitia: “Prietenii mei sunt Larry Larryson, Steven Stevenson si Carol Carolson.” folosind Object.entries()`,
+);
+let sentences = Object.entries(person.friends).reduce(
+  (sentences, [_, friend], index, friends) => {
+    const { name, surname } = friend;
+    const length = friends.length;
+    const punctuation =
+      index === length - 1 ? '.' : index === length - 2 ? ' si ' : ', ';
+
+    return `${sentences}${name} ${surname}${punctuation}`;
+  },
+  'Prietenii mei sunt ',
+);
+console.log(sentences);
+
+console.warn(
+  `In mod similar, afiseaza mai multe propozitii (cate una per console.log()) care sa afiseze: “Larry are xx ani. Steven are …”`,
+);
+Object.entries(person.friends).forEach(([, friend]) => {
+  const { name, age } = friend;
+  console.log(`${name} are ${age} ani.`);
+});
